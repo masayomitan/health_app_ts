95f82a43c025c0ad24f0f0a53ff45f395ce1504d
kawasaki <yomitanmasa3523@gmail.com>
2020-09-12 15:32:41 +0900

    module

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,3 +1,45 @@
 class Score { }
-class Food { }
-class Foods { }
+
+class Food { 
+  constructor(public element: HTMLDivElement){
+    element.addEventListener('click', this.clickEventHandler.bind(this))
+  }
+  clickEventHandler() {
+    this.element.classList.toggle('food--active');
+  }
+}
+
+class Foods { 
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  private _activeElements: HTMLDivElement[] = [];
+  private _activeElementsScore: number[] = [];
+
+  get activeElements() {
+    this._activeElements = [];
+    this.elements.forEach(element => {
+      if(element.classList.contains('food--active')) {
+        this._activeElements.push(element);
+      }
+    })
+    return this._activeElements;
+  }
+
+  get activeElementsScore(){
+    this._activeElementsScore = [];
+    this.activeElements.forEach(element => {
+      const foodScore = element.querySelector('.food__score');
+        if (foodScore){
+          this._activeElementsScore.push(Number(foodScore.textContent));
+        }
+    })
+    return this._activeElementsScore;
+  }
+
+    constructor() {
+      this.elements.forEach(element => {
+        new Food(element);
+      })
+    }
+    
+}
+const foods = new Foods();
